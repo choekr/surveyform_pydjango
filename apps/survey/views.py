3b4cbc19016b0c6864a13329f3f85a281fa7d8d6
Kukrim Choe <choeku86@gmail.com>
2017-03-13 13:52:04 -0700

    functionality complete

--- apps/survey/views.py
+++ apps/survey/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    response = 'Index'
-    return HttpResponse(response)
+    return render(request, "survey/index.html");
+
+def process(request):
+    try:
+        request.session['count'] += 1
+    except Exception:
+        request.session['count'] = 1
+
+    if request.method == 'POST':
+        request.session['name'] = request.POST['name']
+        request.session["location"] = request.POST["location"]
+        request.session["language"] = request.POST["language"]
+        request.session["comment"] = request.POST["comment"]
+        return redirect("/result")
+    else:
+        return redirect('/')
+
+def result(request):
+    return render(request, "survey/result.html")
